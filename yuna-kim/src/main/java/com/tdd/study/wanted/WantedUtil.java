4b61bea2d4000252e03d198ee5ff836ad1545604
chk1511 <chk1511@naver.com>
2019-06-14 17:45:06 +0900

    웹크롤링 test

--- yuna-kim/src/main/java/com/tdd/study/wanted/WantedUtil.java
+++ yuna-kim/src/main/java/com/tdd/study/wanted/WantedUtil.java
@@ -1,9 +1,18 @@
 package com.tdd.study.wanted;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
+import org.apache.tomcat.util.json.JSONParser;
+import org.json.JSONObject;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.DataNode;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class WantedUtil {
 	
@@ -41,7 +50,54 @@ public class WantedUtil {
 		return doc;
 	}
 	
-//	public String getJsonData(Document doc){
-//		
-//	}
+	/**
+	 * json 데이터 가져오기
+	 * @param doc
+	 * @return
+	 */
+	public String getJsonData(Document doc){
+		
+		String jsonStr = null;
+		
+		Elements scriptElements = doc.getElementsByTag("script");
+		
+		for (Element element : scriptElements ){ 
+	        for (DataNode node : element.dataNodes()) {
+	        	String data = node.getWholeData();
+	        	if(data.indexOf("{") != -1){
+	        		jsonStr = data;
+	        	}
+	        }        
+		}
+		
+		return jsonStr;
+	}
+	
+	/**
+	 * tag 리스트 가져오기
+	 * @param str
+	 * @return
+	 * @throws ParseException 
+	 */
+	public String getTagList(String str) throws Exception{
+		
+		JSONObject json = null;
+		json = new JSONObject(str);
+		
+		String props = json.getString("props");
+		json = new JSONObject(props);
+		
+		String serverState = json.getString("serverState");
+		json = new JSONObject(serverState);
+		
+		String core = json.getString("core");
+		json = new JSONObject(core);
+		
+		String tag = json.getString("tag");
+		json = new JSONObject(tag);
+		
+		String industries = json.getString("industries");
+		
+		return industries;
+	}
 }
--- yuna-kim/src/test/java/com/tdd/study/wanted/WantedTest.java
+++ yuna-kim/src/test/java/com/tdd/study/wanted/WantedTest.java
@@ -1,6 +1,8 @@
 package com.tdd.study.wanted;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 import org.jsoup.nodes.DataNode;
 import org.jsoup.nodes.Document;
@@ -8,34 +10,28 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import net.minidev.json.parser.JSONParser;
+
 @SpringBootTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class WantedTest {
 
 	private String url;
 	private Document doc;
+	private WantedUtil w;
 	
-	@Before
-	public void URL생성() {
-		
-		String param = "개발";
-		WantedUtil w = new WantedUtil();
-		w.getUrl(param);
-		this.url = w.getSearchUrl();
-		
-		Assert.assertEquals("https://www.wanted.co.kr/search?query="+param, this.url);
-	}
-	
-	@Test
 	public void 문서가져오기() throws IOException{
-		WantedUtil w = new WantedUtil();
-		w.setSearchUrl(this.url);
-		Document doc = w.getDocument();
+		
+		this.w.setSearchUrl(this.url);
+		this.doc = w.getDocument();
 		
 		// html 태그가 있는지 확인??
 		Elements el = doc.getElementsByTag("html");
@@ -45,31 +41,35 @@ public class WantedTest {
 			result = true;
 		}
 		
-		Assert.assertEquals(true, result);		
+		Assert.assertEquals(true, result);	
 	}
+	
+	@Before
+	public void URL생성() throws IOException {
+		
+		String param = "개발";
+		this.w =  new WantedUtil();
+		w.getUrl(param);
+		this.url = w.getSearchUrl();
+		
+		Assert.assertEquals("https://www.wanted.co.kr/search?query="+param, this.url);
+		
+		문서가져오기();
+	}	
 	
 	@Test
-	public void 제이슨데이터가져오기(){
-		WantedUtil w = new WantedUtil();
-		w.setSearchUrl(this.url);
+	public void A_제이슨데이터가져오기() throws Exception{
 		
+		if(this.doc == null){
+			throw new Exception("문서생성 실패");
+		}
+		
+		String jsonStr = this.w.getJsonData(this.doc);
+		System.out.println(jsonStr);
+		
+		// 태그 리스트 가져오기
+		String str = this.w.getTagList(jsonStr);
+		
+		System.out.println(str);
 	}
-	
-//	@Test
-//	public void 웹크롤링() throws IOException {		
-//		WantedUtil w = new WantedUtil();
-//		Document doc = w.getDocument(this.url);
-//		
-//		Elements scriptElements = doc.getElementsByTag("script");
-//
-//		for (Element element :scriptElements ){ 
-//
-//	        for (DataNode node : element.dataNodes()) {
-//	        	
-//	        	String data = node.getWholeData();
-//	            System.out.println(data.getClass());
-//	        }
-//	        System.out.println("-------------------");            
-//		}
-//	}
 }
